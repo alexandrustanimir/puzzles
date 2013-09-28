de8b1e1172f2d7f3bad1da2dc0cf490f8e286431
alex.stanimir <alexandru.stanimir@live.com>
2013-09-28 17:15:51 +0300

    Remove substring

--- Puzzle/Source.cpp
+++ Puzzle/Source.cpp
@@ -29,7 +29,7 @@ void select_substring(const string &s, int p1, int p2) {
 
 
 /// version 2
-void select_substring(const string &s, int p1, int p2) {
+void select_substring2(const string &s, int p1, int p2) {
     // Write your code here
     // To print results to the standard output you can use the cout stream
     // Example: cout << "Hello world!";
@@ -40,5 +40,18 @@ void select_substring(const string &s, int p1, int p2) {
 		cout<<"Invalid pointers";
 	}
 	cout<< s.substr(p1-1,p2);
+    
+}
+
+
+void remove_substring(const string &s, int p, int n) {
+    // Write your code here
+    // To print results to the standard output you can use the cout stream
+    // Example: cout << "Hello world!";
+
+	if( p>=0 && n <= s.length()- p)
+	{
+		cout<<s.substr(0,p -1 ) << s.substr(p+n-1);
+	}
     
 }
