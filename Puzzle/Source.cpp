c19ba5a2d8a1c622d90c436082f0b74d7f51175d
alex.stanimir <alexandru.stanimir@live.com>
2013-09-28 17:06:54 +0300

    substring

---
+++ Puzzle/Source.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+
+}
+void select_substring(const string &s, int p1, int p2) {
+    // Write your code here
+    // To print results to the standard output you can use the cout stream
+    // Example: cout << "Hello world!";
+
+	int len = s.length();
+	if ( p1 >= len || p2  >= len ||  p1>p2 )
+	{
+		cout<<"Invalid pointers";
+	}
+	for(int i = p1-1 ; i <p2;i++)
+	{
+		cout<<s.at(i);
+	}
+    
+}
