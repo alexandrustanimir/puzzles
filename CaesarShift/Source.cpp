5c9e1eda66e4784b1598647af8df81848ee224c3
Alexandru Stanimir <alex.stanimir@live.com>
2013-12-09 01:16:12 +0200

    add

---
+++ CaesarShift/Source.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <stdio.h>
+using namespace std;
+
+
+int main()
+{
+
+
+}
+
+
+void caesar_shift(const string &s) {
+	// Write your code here
+	// To print results to the standard output you can use the cout stream
+	// Example: cout << "Hello world!";
+	for (string::const_iterator it = s.begin(); it != s.end(); ++it)
+	{
+		//cout<<it
+	}
+}
